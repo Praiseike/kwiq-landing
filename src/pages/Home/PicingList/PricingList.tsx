0fa24264d607a8897416aedeb1f702d32e5a6129
praiseike <praiseike123@gmail.com>
2024-08-27 23:40:20 +0100

    how

--- src/pages/Home/PicingList/PricingList.tsx
+++ src/pages/Home/PicingList/PricingList.tsx
@@ -7,7 +7,7 @@ export default function PriceList() {
       <div className="items-center  inset-0 flex flex-col">
         <div className="sm:w-[50%] mx-auto text-center mt-16">
           <p className="text-[28px] sm:text-[33px] text-white font-[800] mb-5 leading-[40px]">Exchange your crypto like a <br /> boss on KWIQ</p>
-          <p className="text-[16px] font-[500] text-[#EBEFF5]">Deposit, buy, sell, swap & send 50+ crypto seamlessly</p>
+          <p className="text-[16px] font-[500] text-[#EBEFF5] mb-3">Deposit, buy, sell, swap & send 50+ crypto seamlessly</p>
         </div>
         <div className="w-full">
           <CryptoTicker />
