fb80e3ff027d0241af587b62c6e52426420c1382
praiseike <praiseike123@gmail.com>
2024-08-27 18:39:49 +0100

    minor fixes

--- src/pages/Home/PicingList/PricingList.tsx
+++ src/pages/Home/PicingList/PricingList.tsx
@@ -6,7 +6,7 @@ export default function PriceList() {
       {/* <img src="/assets/images/price-background.svg" alt="" /> */}
       <div className="items-center  inset-0 flex flex-col">
         <div className="sm:w-[50%] mx-auto text-center mt-16">
-          <p className="text-[33px] text-white font-[800] mb-5 leading-[40px]">Exchange your crypto like a <br /> boss on KWIQ</p>
+          <p className="text-[28px] sm:text-[33px] text-white font-[800] mb-5 leading-[40px]">Exchange your crypto like a <br /> boss on KWIQ</p>
           <p className="text-[16px] font-[500] text-[#EBEFF5]">Deposit, buy, sell, swap & send 50+ crypto seamlessly</p>
         </div>
         <div className="w-full">
