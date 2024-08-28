23478b4a0f34b1181727194e70ed683d62085472
praiseike <praiseike123@gmail.com>
2024-08-28 16:14:23 +0100

    added graph

--- src/components/CryptoTicker/CryptoTicker.tsx
+++ src/components/CryptoTicker/CryptoTicker.tsx
@@ -1,20 +1,20 @@
 
-import { useEffect, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import Skeleton from 'react-loading-skeleton';
 import 'react-loading-skeleton/dist/skeleton.css';
 import axios from 'axios';
 import btc from '../../assets/coins/image 10.svg';
-import ltc from '../../assets/coins/image 11.svg';
-import doge from '../../assets/coins/image 12.svg';
-import usdt from '../../assets/coins/image 13.svg';
-import eth from '../../assets/coins/image 14.svg';
-import bch from '../../assets/coins/image 15.svg';
-import bnb from '../../assets/coins/image 16.svg';
-import trx from '../../assets/coins/image 17.svg';
-import usdc from '../../assets/coins/image 18.svg';
-import busd from '../../assets/coins/image 19.svg';
-import sol from '../../assets/coins/image 20.svg';
-import aave from '../../assets/coins/image 21.svg';
+import ltc from '../../assets/coins/image 20.svg';
+import doge from '../../assets/coins/image 21.svg';
+import usdt from '../../assets/coins/image 11.svg';
+import eth from '../../assets/coins/image 12.svg';
+import bch from '../../assets/coins/image 13.svg';
+import bnb from '../../assets/coins/image 14.svg';
+import trx from '../../assets/coins/image 15.svg';
+import usdc from '../../assets/coins/image 16.svg';
+import busd from '../../assets/coins/image 17.svg';
+import sol from '../../assets/coins/image 18.svg';
+import aave from '../../assets/coins/image 19.svg';
 
 
 
@@ -23,22 +23,25 @@ interface CoinProps {
   name: string;
   price?: number;
   image: string;
+  symbol?: string;
   percentage?: number;
 }
 
 const coins: CoinProps[] = [
-  { abbr: 'btc', image: btc, name: 'Bitcoin' },
-  { abbr: 'ltc', image: ltc, name: 'Litecoin' },
-  { abbr: "doge", image: doge, name: 'Dogecoin' },
-  { abbr: "usdt", image: usdt, name: 'Tether USDT' },
-  { abbr: "eth", image: eth, name: 'Ethereum' },
-  { abbr: "bch", image: bch, name: 'Bitcoin Cash' },
-  { abbr: "bnb", image: bnb, name: 'BNB' },
-  { abbr: "trx", image: trx, name: 'TRON' },
-  { abbr: "usdc", image: usdc, name: 'USDC' },
-  { abbr: "busd", image: busd, name: 'BUSD' },
-  { abbr: "sol", image: sol, name: 'Solana' },
-  { abbr: "aave", image: aave, name: 'Avalanche' },];
+  { abbr: "btc", image: btc, name: "Bitcoin", symbol: "BINANCE:BTCUSDT" },
+  { abbr: "ltc", image: ltc, name: "Litecoin", symbol: "BINANCE:LTCUSDT" },
+  { abbr: "doge", image: doge, name: "Dogecoin", symbol: "BINANCE:DOGEUSDT" },
+  { abbr: "usdt", image: usdt, name: "Tether USDT", symbol: "BINANCE:USDTUSDT" },
+  { abbr: "eth", image: eth, name: "Ethereum", symbol: "BINANCE:ETHUSDT" },
+  { abbr: "bch", image: bch, name: "Bitcoin Cash", symbol: "BINANCE:BCHUSDT" },
+  { abbr: "bnb", image: bnb, name: "BNB", symbol: "BINANCE:BNBUSDT" },
+  { abbr: "trx", image: trx, name: "TRON", symbol: "BINANCE:TRXUSDT" },
+  { abbr: "usdc", image: usdc, name: "USDC", symbol: "BINANCE:USDCUSDT" },
+  { abbr: "busd", image: busd, name: "BUSD", symbol: "BINANCE:BUSDUSD" },
+  { abbr: "sol", image: sol, name: "Solana", symbol: "BINANCE:SOLUSDT" },
+  { abbr: "aave", image: aave, name: "Avalanche", symbol: "BINANCE:AAVEUSDT" }
+]
+
 
 const transformData = (coinData: any) => {
   if (coinData) {
@@ -50,6 +53,66 @@ const transformData = (coinData: any) => {
   }
   return { price: 1, percentage: 0.00 };
 };
+
+
+
+
+
+const TradingViewWidget = ({ symbol = 'BINANCE:BTCUSDT' }) => {
+  const containerRef = useRef<any>(null);
+  const [loaded, setLoaded] = useState(false);
+
+  useEffect(() => {
+    // Create the script element
+    const script = document.createElement('script');
+    script.src = '/js/trading-hack.js';
+    script.async = true;
+    script.onload = () => setLoaded(true)
+
+    // Set the script's innerHTML with the configuration object
+    script.innerHTML = JSON.stringify({
+      symbol,
+      width: '100%',
+      height: '100%',
+      locale: 'en',
+      dateRange: '1D',
+      colorTheme: 'light',
+      isTransparent: false,
+      autosize: true,
+      largeChartUrl: '',
+      chartOnly: true,
+      noTimeScale: true,
+    });
+
+
+    // Append the script to the container div
+    if (containerRef.current) {
+      containerRef.current.appendChild(script);
+    }
+
+    // Clean up the script on component unmount
+    return () => {
+      if (containerRef.current) {
+
+        containerRef.current.innerHTML = '';
+      }
+    };
+  }, [symbol]);
+
+
+
+  useEffect(() => {
+
+  }, [loaded]);
+
+
+  return (
+    <div className="tradingview-widget-container" ref={containerRef}>
+      <div className="tradingview-widget-container__widget"></div>
+    </div>
+  );
+};
+
 
 const CryptoTicker = () => {
   const [processed, setProcessed] = useState<CoinProps[]>(coins);
@@ -87,7 +150,7 @@ const CryptoTicker = () => {
 
   return (
     <div className="mx-auto w-full md:w-[70%]">
-      <div className="text-black w-full rounded-xl overflow-hidden">
+      <div className="text-black w-full rounded-t-xl pb-12 bg-white overflow-hidden">
         <div className="bg-white overflow-x-auto">
           <table className="min-w-full text-left">
             <thead className="border-b mb-4 border-blue-500">
@@ -109,10 +172,12 @@ const CryptoTicker = () => {
                     {coin.price ? `$${Number(coin.price).toLocaleString()}` : <Skeleton />}
                   </td>
                   <td className={`px-6 py-3 ${coin && coin.percentage && (Number(coin?.percentage) > 0) ? 'text-green-500' : 'text-red-600'}`}>
-                    {coin.percentage ? `${coin.percentage}%` : <Skeleton />}
+                    {coin.percentage !== (null || undefined) ? `${coin.percentage}%` : <Skeleton />}
                   </td>
                   <td className="px-6 py-3 hidden sm:block ">
-                    <div className="h-8 bg-gray-200 rounded-sm"></div>
+                    <div className="h-8 bg-gray-200 rounded-sm w-[12rem]" >
+                      <TradingViewWidget symbol={coin.symbol} />
+                    </div>
                   </td>
                 </tr>
               ))}
--- src/pages/Home/PicingList/PricingList.tsx
+++ src/pages/Home/PicingList/PricingList.tsx
@@ -1,11 +1,16 @@
 import CryptoTicker from "../../../components/CryptoTicker/CryptoTicker";
 import './PricingList.css'
+
+
+
+
 export default function PriceList() {
   return (
     <div className="relative price-list-bg">
       {/* <img src="/assets/images/price-background.svg" alt="" /> */}
       <div className="items-center  inset-0 flex flex-col">
         <div className="sm:w-[50%] mx-auto text-center mt-16">
+
           <p className="text-[28px] sm:text-[33px] text-white font-[800] mb-5 leading-[40px]">Exchange your crypto like a <br /> boss on KWIQ</p>
           <p className="text-[16px] font-[500] text-[#EBEFF5] mb-3">Deposit, buy, sell, swap & send 50+ crypto seamlessly</p>
         </div>
